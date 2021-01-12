781ce1198b8a0df2cd20f9dd4b7a0bb08f84c9e7
EduApps-CDG <eduardoprocopiogomez@gmail.com>
2021-01-12 01:56:10 -0300

    Commit automatizado

--- src/scene/warning-scene.js
+++ src/scene/warning-scene.js
@@ -16,14 +16,18 @@ import * as Global from "../global";
     this.done = false;
     this.animation = null;
 
-    this.title = new PIXI.Text(this.translate["title"],{ fill: 0xFA8916, fontSize: 18 });
-    this.title.anchor.set(0.5, 0.5); //parecido com imagem.pivot
+    this.title = new PIXI.Text(this.translate["title"],{ fill: 0xCC0000, fontSize: 18 }); // 0xFA8916
+    this.title.anchor.set(0.5, 1); //parecido com imagem.pivot
     this.title.position.set(Global.screen.width / 2, 55);
+
+    this.text = new PIXI.Text(this.translate["text"],{ fill: 0xFFFFFF, fontSize: 18 });
+    this.text.anchor.set(0, 0); //parecido com imagem.pivot
+    this.text.position.set(30, 60);
 
     this.popup = new PIXI.NineSlicePlane(Global.loader.resources["./assets/gui/popup.png"].texture,64,64,64,64);
     this.updateSize(1,1);
     this.popup.position.set(Global.screen.width / 2, Global.screen.height / 2);
-    this.popup.tint = 0x555555;
+    this.popup.tint = 0x333333;
 
   }
 
@@ -43,7 +47,7 @@ import * as Global from "../global";
       }
 
       if (this.done) {
-        Global.app.stage.addChild(this.title);
+        Global.app.stage.addChild(this.title,this.text);
         clearInterval(this.animation);
       }
     },Global.screen.FPS60);
